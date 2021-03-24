547f10b704cd83049de4b2e5c478fc5d9cc94427
Sammai Gutierrez <sammaigutierrez@gmail.com>
2021-03-24 14:09:53 -0700

    co-authored-by: Gabriel <mrgabes72@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+// Business logic:
+
+function add(number1, number2) {
+  console.log(number1, number2);
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+// Everything below this line is user interface logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+  const number1 = parseInt($("#add1").val());
+  const number2 = parseInt($("#add2").val());
+  console.log(number1, number2);
+  const result = add(number1, number2);
+  $("#output").text(result);
+  
+  }); 
+});
